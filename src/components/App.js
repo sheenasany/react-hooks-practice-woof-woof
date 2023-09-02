f4451f3b085e892759db6650152724cc82385f9c
Sheena Sang <sheenasany@gmail.com>
2023-09-02 00:09:52 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DogFilter from "./DogFilter";
+import NavBar from "./NavBar";
+import DogCardDisplay from "./DogCardDisplay"
 
 function App() {
+  const [doggos, setDoggos] = useState([])
+  const [featuredDoggo, setFeaturedDoggo] = useState(null)
+  const [goodDogsOnly, setGoodDogsOnly] = useState(false);
+
+useEffect(() => {
+  fetch("http://localhost:3001/pups")
+    .then(res => res.json())
+    .then(data => {
+        setDoggos(data)
+      })
+}, [])
+
+function onUpdateDog(updatedDog) {
+  const updatedDogs = doggos.map((dog) =>
+    dog.id === updatedDog.id ? updatedDog : dog
+  );
+  setDoggos(updatedDogs);
+}
+
+function handleToggleFilter() {
+  setGoodDogsOnly((goodDogsOnly) => !goodDogsOnly);
+}
+
+const selectedDog = doggos.find((dog) => dog.id === featuredDoggo);
+
+let displayDogs = doggos;
+if (goodDogsOnly) {
+  displayDogs = displayDogs.filter((dog) => dog.isGoodDog);
+}
+
   return (
     <div className="App">
-      <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
-      </div>
-      <div id="dog-bar"></div>
-      <div id="dog-summary-container">
-        <h1>DOGGO:</h1>
-        <div id="dog-info"></div>
-      </div>
+      <DogFilter goodDogsOnly={goodDogsOnly} onFilterClick={handleToggleFilter}/>
+      <NavBar doggos={displayDogs} onClickDog={setFeaturedDoggo} />
+      <DogCardDisplay doggo={selectedDog} onUpdateDog={onUpdateDog}   />
     </div>
   );
 }
